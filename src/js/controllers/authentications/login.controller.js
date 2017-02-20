d58bd7c1c06aa2285f472750e6e0e714365740fa
duckot1 <tom.duckworth91@gmail.com>
2017-02-20 15:36:47 +0000

    token factory set up and running for login (setYoken)

--- src/js/controllers/authentications/login.controller.js
+++ src/js/controllers/authentications/login.controller.js
@@ -2,15 +2,15 @@ angular
   .module('jobsApp')
   .controller('LoginCtrl', LoginCtrl);
 
-LoginCtrl.$inject = ['User'];
-function LoginCtrl(User) {
+LoginCtrl.$inject = ['User', 'TokenService'];
+function LoginCtrl(User, TokenService) {
   const vm = this;
 
   vm.login = () => {
     User
       .login(vm.user).$promise
       .then((data) => {
-        console.log(data);
+        TokenService.setToken(data.token);
       }, err => {
         console.log(err);
       });
---
+++ src/js/services/token.service.js
@@ -0,0 +1,13 @@
+angular
+  .module('jobsApp')
+  .service('TokenService', TokenService);
+
+TokenService.$inject = ['$window'];
+function TokenService($window) {
+  const self = this;
+
+  self.setToken = (token) => {
+    return $window.localStorage.setItem('auth-token', token);
+  }
+
+}
