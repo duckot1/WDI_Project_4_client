5a82bb4009b79f5bb3a9d0f22e37b0515c2a6ffe
duckot1 <tom.duckworth91@gmail.com>
2017-02-21 16:32:07 +0000

    delete button functional on users page

--- src/js/controllers/users/show.controller.js
+++ src/js/controllers/users/show.controller.js
@@ -1,19 +1,28 @@
 angular
-  .module('jobsApp')
-  .controller('UsersShowCtrl', UsersShowCtrl);
+.module('jobsApp')
+.controller('UsersShowCtrl', UsersShowCtrl);
 
-UsersShowCtrl.$inject = ['User', '$stateParams'];
+UsersShowCtrl.$inject = ['User', '$stateParams', '$state', 'CurrentUserService'];
 
-function UsersShowCtrl(User, $stateParams) {
+function UsersShowCtrl(User, $stateParams, $state, CurrentUserService) {
   const vm = this;
 
   User
-    .get($stateParams)
-    .$promise
-    .then(data => {
-      console.log(data);
-      vm.user = data;
-    }, err => {
-      console.log(err);
+  .get($stateParams)
+  .$promise
+  .then(data => {
+    console.log(data);
+    vm.user = data;
+  }, err => {
+    console.log(err);
+  });
+
+  vm.delete = function() {
+    User.delete($stateParams).$promise.then(() => {
+      $state.go('login');
+      CurrentUserService.removeUser();
     });
+
+  };
+
 }
