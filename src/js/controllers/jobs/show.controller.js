660198ea1d0c3778a4ae8f65052a6679fc5c8fe4
duckot1 <tom.duckworth91@gmail.com>
2017-02-21 15:15:57 +0000

    delete function added for jobs

--- src/js/controllers/jobs/show.controller.js
+++ src/js/controllers/jobs/show.controller.js
@@ -2,10 +2,15 @@ angular
   .module('jobsApp')
   .controller('JobsShowCtrl', JobsShowCtrl);
 
-JobsShowCtrl.$inject = ['Job', '$stateParams'];
+JobsShowCtrl.$inject = ['Job', '$stateParams', '$state'];
 
-function JobsShowCtrl(Job, $stateParams) {
+function JobsShowCtrl(Job, $stateParams, $state) {
   const vm = this;
   vm.job = Job.get($stateParams);
+  vm.delete = function() {
+    Job.delete($stateParams).$promise.then(() => {
+      $state.go('jobsIndex');
+    });
 
+  };
 }
